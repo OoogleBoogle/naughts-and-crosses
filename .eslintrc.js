4735d054d9dbcbefe9d961cb498e22d3409c6a41
OoogleBoogle <simattfield@gmail.com>
2016-04-22 17:13:30 -0700

    fixed issue where draw could be achieved by clicking on a single box repeatedly

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "standard",
+    "plugins": [
+        "standard"
+    ],
+    "rules": {
+        "indent": ["error", 4],
+        "one-var": ["error", "always"],
+        "brace-style": ["error", "stroustrup"],
+        "semi": ["error", "always"]
+    }
+};
